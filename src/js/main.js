1deeb81520db05a718db094804afb4c9afce8de5
mohamed-saad5411 <124407370+mohamed-saad5411@users.noreply.github.com>
2025-05-25 01:33:46 +0300

    sec commit

--- src/js/main.js
+++ src/js/main.js
@@ -17,15 +17,17 @@ let total = document.getElementById('total')
 
 totalMoney = 0
 function getData() {
-    let salaryCalc = (moneyInp.value * 0.025)
-    salary.innerHTML = salaryCalc.toFixed(2)
-    // let buildCalc = (buildInp.value * 0.025)
-    // build.innerHTML = buildCalc.toFixed(2)
-    // let constructionCalc = (constructionProfitInp.value * 0.025)
-    // construction.innerHTML = constructionCalc.toFixed(2)
-
-    totalMoney = salaryCalc
-    total.innerHTML = totalMoney.toFixed(2)
+    // console.log(moneyInp.value);
+    if (moneyInp.value >= 50) {
+        let salaryCalc = (moneyInp.value * 0.025)
+        salary.innerHTML = salaryCalc.toFixed(2)
+        // let buildCalc = (buildInp.value * 0.025)
+        // build.innerHTML = buildCalc.toFixed(2)
+        // let constructionCalc = (constructionProfitInp.value * 0.025)
+        // construction.innerHTML = constructionCalc.toFixed(2)
+        totalMoney = salaryCalc
+        total.innerHTML = totalMoney.toFixed(2)
+    }
 }
 
 // for register
@@ -34,7 +36,6 @@ let userName = document.getElementById('userName')
 let userEmail = document.getElementById('userEmail')
 let userPhone = document.getElementById('userPhone')
 let userPayment = document.getElementById('userPayment')
-let submitBtn = document.getElementById('submitBtn')
 let getBtn = document.getElementById('getBtn')
 
 let userData = {
@@ -51,14 +52,9 @@ getBtn?.addEventListener('click', function (e) {
     getAllUsers()
 })
 
-submitBtn?.addEventListener('click', function (e) {
-    e.preventDefault()
-    rigester()
-})
-
 async function getAllUsers() {
     // const url = "http://localhost:5000/api/v1/users";
-    const url = "https://apoc3939betest1.vercel.app/api/v1/users";
+    const url = "https://apoc3939betest3.vercel.app/api/v1/users";
     try {
         const response = await fetch(url, {
             method: 'GET',
@@ -74,7 +70,7 @@ async function getAllUsers() {
         users.map((user, index) => {
             cartoona += `<tr><td>${index + 1}</td>
             <td>${user.name}</td>
-                <td>${user.phone}</td>
+                <td>0${user.phone}</td>
                 <td>${user.payment}</td></tr>`
         })
         document.getElementById('userData').innerHTML = cartoona
@@ -83,17 +79,70 @@ async function getAllUsers() {
     }
 }
 
+//---------------------------
+let validUserName = ''
+let validUserEmail = ''
+let validUserPay = ''
+let validUserPhone = ''
+userName.addEventListener('input', () => {
+    function validation() {
+        if (userName.value !== '') {
+            document.querySelector('#userValid').classList.add('d-none')
+            return userName.value
+        }
+        document.querySelector('#userValid').classList.remove('d-none')
+    }
+    validUserName = validation()
+    // console.log(validUserName);
+})
+userEmail.addEventListener('input', () => {
+    let regexEmail = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+    function validation() {
+        if (userEmail.value !== '' && regexEmail.test(userEmail.value)) {
+            document.querySelector('#emailValid').classList.add('d-none')
+            return userEmail.value
+        }
+        document.querySelector('#emailValid').classList.remove('d-none')
+    }
+    validUserEmail = validation()
+    // console.log(validUserName);
+})
+userPhone.addEventListener('input', () => {
+    let regexpNum = /^01[0-2,5]{1}[0-9]{8}$/;
+    function validation() {
+        if (userPhone.value !== '' && regexpNum.test(userPhone.value)) {
+            document.querySelector('#phoneValid').classList.add('d-none')
+            return userPhone.value
+        }
+        document.querySelector('#phoneValid').classList.remove('d-none')
+    }
+    validUserPhone = validation()
+    // console.log(validUserName);
+})
+userPayment.addEventListener('input', () => {
+    function validation() {
+        if (userPayment.value !== '') {
+            document.querySelector('#paymentValid').classList.add('d-none')
+            return userPayment.value
+        }
+        document.querySelector('#paymentValid').classList.remove('d-none')
+    }
+    validUserPay = validation()
+    // console.log(validUserName);
+})
+//---------------------------
+
 async function rigester() {
     // const url = "http://localhost:5000/api/v1/register";
-    const url = "https://apoc3939betest1.vercel.app/api/v1/register";
+    const url = "https://apoc3939betest3.vercel.app/api/v1/register";
     try {
         const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
-                name: userName.value,
-                email: userEmail.value,
-                phone: userPhone.value,
-                payment: userPayment.value,
+                name: validUserName,
+                email: validUserEmail,
+                phone: validUserPhone,
+                payment: validUserPay,
             }),
             headers: {
                 "Content-Type": "application/json",
@@ -118,14 +167,15 @@ async function rigester() {
 let paymentBtn = document.getElementById('paymentBtn')
 paymentBtn?.addEventListener('click', async (e) => {
     e.preventDefault()
-    console.log(e);
+    rigester()
+    // console.log(e);
 
-    await fetch('https://apoc3939betest1.vercel.app/api/v1/register/create-checkout-session/682649627d11fb0aa148216a', {
+    await fetch('https://apoc3939betest3.vercel.app/api/v1/register/create-checkout-session/682649627d11fb0aa148216a', {
         method: 'POST',
         body: JSON.stringify({
-            name: userName.value,
-            email: userEmail.value,
-            payment: userPayment.value,
+            name: validUserName,
+            email: validUserEmail,
+            payment: validUserPay,
         }),
         headers: {
             "Content-Type": "application/json",
@@ -201,5 +251,18 @@ $(window).scroll(function () {
 
 });
 
-// ********************* // 
+// progress bar
+let progress = document.querySelector('.progress')
+let progressBar = document.querySelector('.progress-bar')
+
+function calcProgressWidth() {
+    let collectMoney = Number(document.querySelector('#collectMoney').innerHTML)
+    let totalMoney = Number(document.querySelector('#totalMoney').innerHTML)
+    progressBar.style.width = `${collectMoney * 100 / totalMoney}%`
+    progressBar.innerHTML = `${Math.ceil(collectMoney * 100 / totalMoney)}%`
+    console.log();
+
+}
+calcProgressWidth()
+
 
